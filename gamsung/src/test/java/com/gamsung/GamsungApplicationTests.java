428a652b22eab4be2e54716dc2869c57f1e28ed5
Su Jung Jo <zwleotkfkdz@naver.com>
2019-09-20 17:34:30 +0900

    project start
    -by sujung

---
+++ gamsung/src/test/java/com/gamsung/GamsungApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gamsung;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamsungApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
