60bb61d294b226a5f7501032d9ae4b8836e78d7f
Su Jung Jo <zwleotkfkdz@naver.com>
2019-09-24 15:23:56 +0900

    deal
    -by sujung

--- src/main/java/com/gamsung/controller/DealController.java
+++ src/main/java/com/gamsung/controller/DealController.java
@@ -5,22 +5,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.gamsung.service.DealService;
 import com.gamsung.service.ProductService;
+import com.gamsung.vo.Deal;
 import com.gamsung.vo.Product;
 
 @Controller
 @RequestMapping(path = "/deal")
 public class DealController {
+
 	@Autowired
 	ProductService productService;
 
+	@Autowired
+	DealService dealService;
+
 	@GetMapping(path = "/{productNo}")
 	public String dealForm(@PathVariable int productNo, Model model) {
-		System.out.println(productNo);
 		Product product = productService.findProductByProductNo(productNo);
 		model.addAttribute("product", product);
 		return "deal/deal";
+	}
+
+	@PostMapping(path = "/order")
+	public String dealForm(Deal deal) {
+		deal.setBuyer("sj");
+		dealService.registerDeal(deal);
+		return "redirect:/home";
 	}
 }
